c3476dad031a35ab1d0e0c74eddd4d25e0731b06
Erasmo Leite <erasmo.ldasilva@hotmail.com>
2021-09-18 13:14:35 -0300

    Atualização

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -2,7 +2,6 @@
 package intro;
 
 // 2- Referência Bibliotecas
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 // 3- Classe
 public class Medidas {
@@ -10,7 +9,12 @@ public class Medidas {
 
     //3.2 Métodos e Funções
     public static void main(String[] arg) {
-        System.out.println("Cálculo de Areas!!!");
+        calcularAreaModoCurto();
+        calcularAreaModoExtenso();
+
+    }
+    public static void calcularAreaModoCurto(){
+        System.out.println("Cálculo de Areas Modo Curto!!!");
 
         // Calculo de área - Reduzido
         int largura = 4;
@@ -21,7 +25,10 @@ public class Medidas {
                 + comprimento + "m a área é de " + largura * comprimento + " m²");
 
     }
-    public void calcularAreaModoExtenso(){
+
+    public static void calcularAreaModoExtenso(){
+
+        System.out.println("Cálculo de Areas Modo Extenso!!!");
     //Calculo de área - Exemplo: o tamanho do tapete ou do piso
         int largura;
         int comprimento;
@@ -33,15 +40,6 @@ public class Medidas {
         System.out.println("Para a largura de " + largura + "m e o comprimento de "
                 + comprimento + "m a área é de " + resultado + " m²");
 
-    }
-    public void calcularAreaModoCompacto(){
-        // Calculo de área - Reduzido
-        int largura = 4;
-        int comprimento = 3;
-
-        System.out.println();
-        System.out.println("Para a largura de " + largura + "m e o comprimento de "
-                + comprimento + "m a área é de " + largura * comprimento + " m²");
 
     }
 
